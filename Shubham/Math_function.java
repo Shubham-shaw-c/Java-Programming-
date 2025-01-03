e9b98f7660ba6366e2d0638964075e15e79b8243
Shubham-shaw-c <shubham.shaw@stu.adamasuniversity.ac.in>
2025-01-03 19:51:48 +0530

    Math functions.

---
+++ Shubham/Math_function.java
@@ -0,0 +1,13 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+package Shubham;
+
+public class Math_function {
+   public Math_function() {
+   }
+
+   public static void main(String[] args) {
+      System.out.println(Math.min(5, 6));
+      System.out.println(Math.max(5, 6));
+      System.out.println(Math.random());
+   }
+}
